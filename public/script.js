d974e55bd8ec273fe4a9f362c3aaab614b8d8003
Joel Stoker <truesilver92@gmail.com>
2019-03-07 10:33:29 -0700

    finished

--- public/script.js
+++ public/script.js
@@ -1,63 +1,85 @@
 var app = new Vue({
-  el: '#app',
-  data: {
-    items: [{
-      text: "make an app",
-      completed: false,
-    }, {
-      text: "declare victory",
-      completed: false,
-    }, {
-      text: "profit",
-      completed: false
-    }],
-    text: '',
-    show: 'all',
-  },
-  computed: {
-    activeItems() {
-      return this.items.filter(item => {
-        return !item.completed;
-      });
+    el: '#app',
+    data: {
+        items: [],
+        text: '',
+        show: 'all',
     },
-    filteredItems() {
-      if (this.show === 'active')
-        return this.items.filter(item => {
-          return !item.completed;
-        });
-      if (this.show === 'completed')
-        return this.items.filter(item => {
-          return item.completed;
-        });
-      return this.items;
+    created: function() {
+        this.getItems();
     },
-  },
-  methods: {
-    addItem() {
-      this.items.push({
-        text: this.text,
-        completed: false
-      });
-      this.text = '';
+    computed: {
+        activeItems() {
+            return this.items.filter(item => {
+                return !item.completed;
+            });
+        },
+        filteredItems() {
+            if (this.show === 'active')
+                return this.items.filter(item => {
+                    return !item.completed;
+                });
+            if (this.show === 'completed')
+                return this.items.filter(item => {
+                    return item.completed;
+                });
+            return this.items;
+        },
     },
-    deleteItem(item) {
-      var index = this.items.indexOf(item);
-      if (index > -1)
-        this.items.splice(index, 1);
-    },
-    showAll() {
-      this.show = 'all';
-    },
-    showActive() {
-      this.show = 'active';
-    },
-    showCompleted() {
-      this.show = 'completed';
-    },
-    deleteCompleted() {
-      this.items = this.items.filter(item => {
-        return !item.completed;
-      });
-    },
-  }
+    methods: {
+        async completeItem(item) {
+            try {
+                const response = axios.put("/api/items/" + item.id, {
+                    text: item.text,
+                    completed: !item.completed,
+                });
+                this.getItems();
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        async addItem() {
+            try {
+                const response = await axios.post("/api/items", {
+                    text: this.text,
+                    completed: false
+                });
+                this.text = "";
+                this.getItems();
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        async deleteItem(item) {
+            try {
+                const response = await axios.delete("/api/items/" + item.id);
+                this.getItems();
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        async getItems() {
+            try {
+                const response = await axios.get("/api/items");
+                this.items = response.data;
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        showAll() {
+            this.show = 'all';
+        },
+        showActive() {
+            this.show = 'active';
+        },
+        showCompleted() {
+            this.show = 'completed';
+        },
+        deleteCompleted() {
+            this.items.forEach(item => {
+                if (item.completed)
+                    this.deleteItem(item);
+            });
+        },
+    }
 });
---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+let items = [];
+let id = 0;
+app.post('/api/items', (req, res) => {
+  id = id + 1;
+  let item = {
+    id: id,
+    text: req.body.text,
+    completed: req.body.completed
+  };
+  items.push(item);
+  res.send(item);
+});
+app.get('/api/items', (req, res) => {
+  res.send(items);
+});
+app.put('/api/items/:id', (req, res) => {
+  let id = parseInt(req.params.id);
+  let itemsMap = items.map(item => {
+    return item.id;
+  });
+  let index = itemsMap.indexOf(id);
+  if (index === -1) {
+    res.status(404)
+      .send("Sorry, that item doesn't exist");
+    return;
+  }
+  let item = items[index];
+  item.text = req.body.text;
+  item.completed = req.body.completed;
+  res.send(item);
+});
+app.delete('/api/items/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+    let removeIndex = items.map(item => {
+        return item.id;
+    })
+        .indexOf(id);
+    if (removeIndex === -1) {
+        res.status(404)
+            .send("Sorry, that item doesn't exist");
+        return;
+    }
+    items.splice(removeIndex, 1);
+    res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log('Server listening!'));
